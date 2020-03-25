6ca91562a6b1aed9b2b087d83a83029152bb6349
Rusty Key <iam@stfoo.ru>
2020-03-24 19:46:38 -0700

    untangle Setup.Bsb.runSetupChain

--- src/Setup.ml
+++ src/Setup.ml
@@ -1,6 +1,13 @@
 open Bindings
 module P = Js.Promise
 
+(* Why the are progress percentages hardcoded the way they are?
+
+   Azure's REST API doesn't set the file size of the zip file in the headers.
+   This makes it hard to report download progress correctly. We work around this
+   by assuming that the download process takes up 80% of the setup time and
+   assign some approximate hardcoded time for the rest of the setups *)
+
 module Internal = struct
   type t (* event emitter *)
 
@@ -46,128 +53,122 @@ module Bsb = struct
     RequestProgress.onError stream error;
     RequestProgress.pipe stream (Fs.createWriteStream file)
 
-  let dropAnEsyJSON path =
-    Bindings.Fs.writeFile path Bindings.thisProjectsEsyJson
+  let dropEsyJSON path = Bindings.Fs.writeFile path Bindings.thisProjectsEsyJson
 
   let make () = makeEventEmitter ()
 
-  (* Why the are progress percentages hardcoded the way they are?
+  let createEsyFolder ~esyRoot =
+    Fs.mkdir ~p:true esyRoot
+    |> P.then_ (function
+         | Error Fs.E.PathNotFound ->
+           Error
+             {j| Setup failed with because of invalid path provided to it: $esyRoot |j}
+           |> P.resolve
+         | Ok () -> Ok () |> P.resolve)
 
-   Azure's REST API doesn't set the file size of the zip file in the headers.
-   This makes it hard to report download progress correctly. We work around this
-   by assuming that the download process takes up 80% of the setup time and
-   assign some approximate hardcoded time for the rest of the setups *)
+  let writeEsyJson ~esyRoot =
+    Filename.concat esyRoot "esy.json"
+    |> dropEsyJSON
+    |> P.then_ (fun () -> Ok () |> P.resolve)
+
+  let installDepsWithEsy ~esyRoot ~esyCmd =
+    Cmd.output esyCmd ~cwd:esyRoot ~args:[| "install"; "-P"; esyRoot |]
+    |> P.then_ (function
+         | Error msg ->
+           Error ("'esy install' failed. Reason: " ^ msg) |> P.resolve
+         | Ok _stdout -> Ok () |> P.resolve)
+
+  let getArtifactsUrl ~eventEmitter =
+    reportProgress eventEmitter 0.1;
+    AzurePipelines.getBuildID ()
+    |> P.then_ (function
+         | Error msg ->
+           Error {j| Azure artifacts cache failure: $msg |j} |> P.resolve
+         | Ok id ->
+           id |> AzurePipelines.getDownloadURL
+           |> P.then_ (function
+                | Error msg ->
+                  Error {j| Azure artifacts cache failure: $msg |j} |> P.resolve
+                | Ok url -> Ok url |> P.resolve))
+
+  let downloadArtifacts ~esyRoot ~eventEmitter url =
+    Js.Console.info2 "download" url;
+    let lastProgress = ref 0.0 in
+    P.make (fun ~resolve ~reject:_ ->
+        download url
+          (Path.join [| esyRoot; "cache.zip" |])
+          ~progress:(fun progressFraction ->
+            (* The cache restoration is assumed to take up 80%. More
+               explanation at the top *)
+            let percent = progressFraction *. 80.0 in
+            reportProgress eventEmitter (percent -. !lastProgress);
+            lastProgress := percent)
+          ~data:(fun _ -> ())
+          ~error:(fun _ ->
+            (resolve (Error "Azure artifacts cache failure: ") [@bs]))
+          ~end_:(fun () ->
+            (* See comment at the top explaining 96.66 *)
+            reportProgress eventEmitter 93.33;
+            (resolve (Ok ()) [@bs])))
+
+  let unzipArtifacts ~esyRoot ~envWithUnzip =
+    Cmd.make ~cmd:"unzip" ~env:envWithUnzip
+    |> P.then_ (function
+         | Error msg -> P.resolve (Error msg)
+         | Ok unzip -> Cmd.output unzip ~args:[| "cache.zip" |] ~cwd:esyRoot)
+    |> P.then_ (function
+         | Error unzipCmdError ->
+           Error {j| Azure artifacts cache failure: $unzipCmdError |j}
+           |> P.resolve
+         | Ok _unzipCmdOutput -> P.resolve (Ok ()))
+
+  let importDownloadedDependencies ~esyRoot ~esyCmd =
+    Cmd.output esyCmd ~cwd:esyRoot
+      ~args:[| "import-dependencies"; "-P"; esyRoot |]
+    |> P.then_ (function
+         | Error msg ->
+           Error ("'esy import-dependencies' failed. Reason: " ^ msg)
+           |> P.resolve
+         | Ok _ -> Ok () |> P.resolve)
+
+  let buildDependencies ~esyRoot ~esyCmd ~eventEmitter =
+    reportProgress eventEmitter 99.99;
+    (* See comment at the top explaining the 99.99 *)
+    Cmd.output esyCmd ~cwd:esyRoot ~args:[| "build"; "-P"; esyRoot |]
+    |> P.then_ (function
+         | Error msg ->
+           Error ("'esy build' failed. Reason: " ^ msg) |> P.resolve
+         | Ok _esyCmdOutput -> Ok () |> P.resolve)
 
   let runSetupChain esyCmd envWithUnzip eventEmitter folder =
     let open Bindings in
-    let hiddenEsyRoot = Path.join [| folder; ".vscode"; "esy" |] in
-    Rimraf.run hiddenEsyRoot
+    let esyRoot = Path.join [| folder; ".vscode"; "esy" |] in
+    Rimraf.run esyRoot
     |> P.then_ (function
          | Error () ->
-           Error
-             {j| Rimraf failed before the bsb toolchain setup: $hiddenEsyRoot |j}
+           Error {j| Rimraf failed before the bsb toolchain setup: $esyRoot |j}
            |> P.resolve
-         | Ok () ->
-           Fs.mkdir ~p:true hiddenEsyRoot
-           |> P.then_ (function
-                | Ok () -> Ok () |> P.resolve
-                | Error Fs.E.PathNotFound ->
-                  Error
-                    {j| Setup failed with because of invalid path provided to it: $hiddenEsyRoot |j}
-                  |> P.resolve))
+         | Ok () -> createEsyFolder ~esyRoot)
     |> P.then_ (function
          | Error e -> Error e |> P.resolve
-         | Ok () ->
-           Filename.concat hiddenEsyRoot "esy.json"
-           |> dropAnEsyJSON
-           |> P.then_ (fun () -> P.resolve (Ok ())))
+         | Ok () -> writeEsyJson ~esyRoot)
     |> P.then_ (function
          | Error e -> Error e |> P.resolve
-         | Ok () ->
-           Cmd.output esyCmd ~cwd:hiddenEsyRoot
-             ~args:[| "install"; "-P"; hiddenEsyRoot |]
-           |> P.then_ (function
-                | Error msg ->
-                  Error ("'esy install' failed. Reason: " ^ msg) |> P.resolve
-                | Ok _stdout ->
-                  reportProgress eventEmitter 0.1;
-                  (* See comment at the bottom *)
-                  AzurePipelines.getBuildID ()
-                  |> P.then_ (function
-                       | Ok id ->
-                         id |> AzurePipelines.getDownloadURL
-                         |> P.then_ (function
-                              | Ok url -> P.resolve (Ok url)
-                              | Error msg ->
-                                Error
-                                  {j| Azure artifacts cache failure: $msg |j}
-                                |> P.resolve)
-                       | Error msg ->
-                         Error {j| Azure artifacts cache failure: $msg |j}
-                         |> P.resolve)
-                  |> P.then_ (function
-                       | Ok url -> Ok url |> P.resolve
-                       | Error e -> Error e |> P.resolve)))
+         | Ok () -> installDepsWithEsy ~esyRoot ~esyCmd)
+    |> P.then_ (fun _ -> getArtifactsUrl ~eventEmitter)
     |> P.then_ (function
+         | Error cacheFailureReason -> P.resolve (Error cacheFailureReason)
          | Ok downloadUrl ->
-           Js.log2 "download" downloadUrl;
-           let lastProgress = ref 0.0 in
-           P.make (fun ~resolve ~reject:_ ->
-               download downloadUrl
-                 (Path.join [| hiddenEsyRoot; "cache.zip" |])
-                 ~progress:(fun progressFraction ->
-                   let percent = progressFraction *. 80.0 in
-                   (* The cache restoration is assumed to take up 80%. More
-                      explanation at the bottom *)
-                   reportProgress eventEmitter (percent -. !lastProgress);
-                   lastProgress := percent)
-                 ~data:(fun _ -> ())
-                 ~error:(fun _ ->
-                   (resolve (Error "Azure artifacts cache failure: ") [@bs]))
-                 ~end_:(fun () -> (resolve (Ok ()) [@bs])))
-         | Error cacheFailureReason -> P.resolve (Error cacheFailureReason))
-    |> P.then_ (function
-         | Ok () ->
-           reportProgress eventEmitter 93.33;
-           (* See comment at the bottom explaining 93.33 *)
-           Cmd.make ~cmd:"unzip" ~env:envWithUnzip
-           |> P.then_ (function
-                | Error msg -> P.resolve (Error msg)
-                | Ok unzip ->
-                  Cmd.output unzip ~args:[| "cache.zip" |] ~cwd:hiddenEsyRoot)
-           |> P.then_ (function
-                | Error unzipCmdError ->
-                  Error {j| Azure artifacts cache failure: $unzipCmdError |j}
-                  |> P.resolve
-                | Ok _unzipCmdOutput -> P.resolve (Ok ()))
-         | Error e -> Error e |> P.resolve)
-    |> P.then_ (function
-         | Ok () ->
-           reportProgress eventEmitter 96.66;
-           (* See comment at the bottom explaining 96.66 *)
-           Cmd.output esyCmd ~cwd:hiddenEsyRoot
-             ~args:[| "import-dependencies"; "-P"; hiddenEsyRoot |]
-           |> P.then_ (fun r ->
-                  P.resolve
-                    ( match r with
-                    | Ok _ -> Ok ()
-                    | Error msg ->
-                      Error ("'esy import-dependencies' failed. Reason: " ^ msg)
-                    ))
-         | Error e -> P.resolve (Error e))
+           downloadArtifacts ~esyRoot ~eventEmitter downloadUrl)
     |> P.then_ (function
          | Error e -> Error e |> P.resolve
-         | Ok () ->
-           reportProgress eventEmitter 99.99;
-           (* See comment at the bottom explaining the 99.99 *)
-           Cmd.output esyCmd ~cwd:hiddenEsyRoot
-             ~args:[| "build"; "-P"; hiddenEsyRoot |]
-           |> P.then_ (fun r ->
-                  P.resolve
-                    ( match r with
-                    | Ok _esyCmdOutput -> Ok ()
-                    | Error msg -> Error ("'esy build' failed. Reason: " ^ msg)
-                    )))
+         | Ok () -> unzipArtifacts ~esyRoot ~envWithUnzip)
+    |> P.then_ (function
+         | Error e -> Error e |> P.resolve
+         | Ok () -> importDownloadedDependencies ~esyRoot ~esyCmd)
+    |> P.then_ (function
+         | Error e -> Error e |> P.resolve
+         | Ok () -> buildDependencies ~esyRoot ~esyCmd ~eventEmitter)
 
   let run esyCmd esyEnv eventEmitter projectPath =
     let projectPath = Path.join [| projectPath; ".."; ".." |] in
