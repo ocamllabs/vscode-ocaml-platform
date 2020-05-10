c1c4e44bc5b28a90ff1e9cfabab422cba074f363
Sora Morimoto <git@bsky.moe>
2020-05-11 08:24:51 +0900

    Tweak print_esy_cache.js

--- .github/workflows/print_esy_cache.js
+++ .github/workflows/print_esy_cache.js
@@ -5,9 +5,11 @@ const path = require("path");
 const ESY_FOLDER = process.env.ESY__PREFIX
   ? process.env.ESY__PREFIX
   : path.join(os.homedir(), ".esy");
+
 const esy3 = fs
   .readdirSync(ESY_FOLDER)
   .filter((name) => name.length > 0 && name[0] === "3")
   .sort()
   .pop();
-console.log(`::set-output name=esy_cache::${path.join(ESY_FOLDER, esy3, "i")}`);
+
+console.log(`::set-output name=dir::${path.join(ESY_FOLDER, esy3, "i")}`);
