2524257cc6414de1fa96e3537ee810042e943be1
Manas <prometheansacrifice@gmail.com>
2020-05-10 18:09:50 +0000

    Fixes e2e tests for esy (#190)
    * Fixes e2e tests for esy
    
    * Log esy status
    
    * Run tests after packaging it
    
    * Setup up fixtures before running vscode instance
    
    * temporarily add --verbose to e2e tests
    
    * Increase delay to see if the test picks the diagnostics
    
    * Update runEsyTests.js

--- test/runEsyTests.js
+++ test/runEsyTests.js
@@ -1,9 +1,29 @@
 const path = require("path");
+const os = require("os");
+const fs = require("fs-extra");
+const cp = require("child_process");
 
 const { runTests } = require("vscode-test");
 
+let root = path.dirname(__dirname);
+let sampleEsySrc = path.join(root, "fixtures", "sample-esy");
+let projectPath = path.join(os.tmpdir(), "sample-esy");
+
 async function main() {
   try {
+    fs.copySync(sampleEsySrc, projectPath);
+    console.log("Running in", projectPath);
+    cp.execSync("esy", { cwd: projectPath });
+
+    fs.writeFileSync(
+      path.join(projectPath, ".vscode", "settings.json"),
+      JSON.stringify({
+        "ocaml.sandbox": {
+          root: projectPath,
+          kind: "esy",
+        },
+      })
+    );
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
     const extensionDevelopmentPath = path.resolve(__dirname, "../");
