589ca44355aa269a0bd13dbf103f91c6121dde52
Rudi Grinberg <me@rgrinberg.com>
2020-05-22 15:26:27 -0700

    Proper json parsing of experimental capabilities
    Also add warning if capabilities cannot be parsed
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/ocamlLsp.ml
+++ src/ocamlLsp.ml
@@ -4,19 +4,23 @@ type t = { interfaceSpecificLangId : bool }
 
 let default = { interfaceSpecificLangId = false }
 
+let ofJson (json : Js.Json.t) =
+  let open Json.Decode in
+  try
+    let interfaceSpecificLangId = field "interfaceSpecificLangId" bool json in
+    { interfaceSpecificLangId }
+  with DecodeError _ ->
+    message `Warn
+      "unexpected experimental capabilities from lsp server. Some features \
+       might be missing";
+    default
+
 let ofInitializeResult (t : Vscode.LanguageClient.InitializeResult.t) =
   match Js.Nullable.toOption t.capabilities.experimental with
   | None -> default
   | Some json -> (
-    match Js.Json.decodeObject json with
-    | None -> default
-    | Some o ->
-      let interfaceSpecificLangId =
-        match Js.Dict.get o "interfaceSpecificLangId" with
-        | None -> false
-        | Some json ->
-          Belt.Option.getWithDefault (Js.Json.decodeBoolean json) false
-      in
-      { interfaceSpecificLangId } )
+    match Json.Decode.field "ocamllsp" ofJson json with
+    | s -> s
+    | exception Json.Decode.DecodeError _ -> default )
 
 let interfaceSpecificLangId t = t.interfaceSpecificLangId
