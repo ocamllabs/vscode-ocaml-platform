0800990e96b6585c863d777583f54301743946be
Max Lantas <25037249+mnxn@users.noreply.github.com>
2020-08-16 20:01:05 -0700

    remove dead code

--- src/AzurePipelines.mli
+++
@@ -1,7 +0,0 @@
-(** Provides functions for fetching prebuilt artifacts from Azure Pipelines *)
-
-(** Fetches id of the latest build *)
-val getBuildId : unit -> (int, string) result Promise.t
-
-(** Fetches download url for the given build id *)
-val getDownloadUrl : int -> (string, string) result Promise.t
