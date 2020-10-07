f9b046ebe701d62f68ba03779e267336b7a81cc7
Max Lantas <mnxndev@outlook.com>
2020-10-07 11:51:20 -0700

    open import in path module

--- src/path.ml
+++ src/path.ml
@@ -1,3 +1,5 @@
+open Import
+
 type t = string
 
 let equal = String.equal
@@ -6,13 +8,16 @@ let of_string s = s
 
 let to_string s = s
 
-let is_absolute t = not (Filename.is_relative t)
+let is_absolute t = Filename.is_absolute t
 
 let compare = String.compare
 
 let dirname = Filename.dirname
 
-let extname t = Filename.extension t
+let extname t =
+  match Filename.split_extension t with
+  | _, Some ext -> ext
+  | _, None -> ""
 
 let basename t = Filename.basename t
 
@@ -20,7 +25,7 @@ let ( / ) = Filename.concat
 
 let relative = ( / )
 
-let relative_all p xs = List.fold_left Filename.concat p xs
+let relative_all p xs = List.fold_left xs ~f:Filename.concat ~init:p
 
 let join x y = Filename.concat x y
 
@@ -28,7 +33,7 @@ let with_ext x ~ext = x ^ ext
 
 let is_root = function
   | "" -> true
-  | x -> Filename.dirname x = x
+  | x -> String.equal (Filename.dirname x) x
 
 let parent x =
   if is_root x then
