407a8095e46ecd850c8d42299afc3a4b87ba53de
artemiy <artemiy.rozovyk@etu.upmc.fr>
2021-07-12 11:46:32 +0200

    Update AST output onDidChangeTextDocument

--- src/ast_editor.ml
+++ src/ast_editor.ml
@@ -4,6 +4,11 @@ let read_html_file () =
   let filename = Node.__dirname () ^ "/../astexplorer/dist/index.html" in
   Fs.readFile filename
 
+let document_eq a b =
+  String.equal
+    (Uri.toString (TextDocument.uri a) ())
+    (Uri.toString (TextDocument.uri b) ())
+
 let send_msg t value ~(webview : WebView.t) =
   let msg = Ojs.empty_obj () in
   Ojs.set_prop_ascii msg "type" (Ojs.string_to_js t);
@@ -18,10 +23,29 @@ let transform_to_ast ~(document : TextDocument.t) ~(webview : WebView.t) =
   in
   send_msg "parse" (Jsonoo.t_to_js ast_js) ~webview
 
+let onDidChangeTextDocument_listener event ~(document : TextDocument.t)
+    ~(webview : WebView.t) =
+  let changed_document = TextDocumentChangeEvent.document event in
+  if document_eq document changed_document then
+    transform_to_ast ~document ~webview
+  else
+    ()
+
 let resolveCustomTextEditor ~(document : TextDocument.t) ~webviewPanel ~token:_
     : CustomTextEditorProvider.ResolvedEditor.t =
   let _ = document in
   let webview = WebviewPanel.webview webviewPanel in
+  let onDidChangeTextDocument_disposable =
+    Workspace.onDidChangeTextDocument
+      ~listener:(onDidChangeTextDocument_listener ~webview ~document)
+      ()
+  in
+  let _ =
+    WebviewPanel.onDidDispose webviewPanel
+      ~listener:(fun () ->
+        Disposable.dispose onDidChangeTextDocument_disposable)
+      ()
+  in
   transform_to_ast ~document ~webview;
   let options = WebView.options webview in
   WebviewOptions.set_enableScripts options true;
