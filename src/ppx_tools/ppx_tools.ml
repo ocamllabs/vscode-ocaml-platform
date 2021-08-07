464607739cb2bcbef53cd9be71aa1267c8ee56c6
Rudi Grinberg <me@rgrinberg.com>
2021-08-06 19:20:48 -0700

    Move shorter clause higher
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/ppx_tools/ppx_tools.ml
+++ src/ppx_tools/ppx_tools.ml
@@ -7,6 +7,7 @@ let get_preprocessed_ast path = Ppxlib.Ast_io.read_binary path
 
 let get_reparsed_code_from_pp_file ~document =
   match Pp_path.get_pp_path ~document with
+  | None -> Error "Error : path to preprocessed document wasn't found"
   | Some pp_path -> (
     match get_preprocessed_ast pp_path with
     | Ok res ->
@@ -17,4 +18,3 @@ let get_reparsed_code_from_pp_file ~document =
         | Intf signature ->
           Caml.Format.asprintf "%a" Pprintast.signature signature)
     | Error err_msg -> Error err_msg)
-  | None -> Error "Error : path to preprocessed document wasn't found"
