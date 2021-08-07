04f0697357c1ae93be8430164081dd1ebd170d27
Rudi Grinberg <me@rgrinberg.com>
2021-08-06 19:21:58 -0700

    Simplify with Result.map
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- src/ppx_tools/ppx_tools.ml
+++ src/ppx_tools/ppx_tools.ml
@@ -1,3 +1,4 @@
+open Base
 module Traverse_ast = Traverse_ast
 module Traverse_ast2 = Traverse_ast2
 module Dumpast = Dumpast
@@ -8,13 +9,11 @@ let get_preprocessed_ast path = Ppxlib.Ast_io.read_binary path
 let get_reparsed_code_from_pp_file ~document =
   match Pp_path.get_pp_path ~document with
   | None -> Error "Error : path to preprocessed document wasn't found"
-  | Some pp_path -> (
-    match get_preprocessed_ast pp_path with
-    | Ok res ->
-      Ok
-        (match Ppxlib.Ast_io.get_ast res with
-        | Impl structure ->
-          Caml.Format.asprintf "%a" Pprintast.structure structure
-        | Intf signature ->
-          Caml.Format.asprintf "%a" Pprintast.signature signature)
-    | Error err_msg -> Error err_msg)
+  | Some pp_path ->
+    get_preprocessed_ast pp_path
+    |> Result.map ~f:(fun res ->
+           match Ppxlib.Ast_io.get_ast res with
+           | Impl structure ->
+             Caml.Format.asprintf "%a" Pprintast.structure structure
+           | Intf signature ->
+             Caml.Format.asprintf "%a" Pprintast.signature signature)
