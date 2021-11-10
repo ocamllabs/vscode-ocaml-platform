aa42b0dd6fdd309a40034532ff650288cce4ee66
Ulugbek Abdullaev <ulugbekna@gmail.com>
2021-11-10 19:20:20 +0100

    add `Ocaml` module to store its version

--- src/ocaml.ml
+++ src/ocaml.ml
@@ -1,2 +1,17 @@
-let version sandbox =
-  Sandbox.get_command sandbox "ocamlc" [ "--version" ] |> Cmd.output
+open Import
+
+type t = Ocaml_version.t
+
+let make sandbox =
+  let open Promise.Syntax in
+  let+ r = Sandbox.get_command sandbox "ocamlc" [ "--version" ] |> Cmd.output in
+  match r with
+  | Ok v ->
+    Ocaml_version.of_string v
+    |> Result.map_error ~f:(function `Msg _ -> `Unable_to_parse_version v)
+  | Error e ->
+    log_chan ~section:"Ocaml.version_semver" `Warn
+      "Error running `ocamlc --version`: %s" e;
+    Error `Ocamlc_missing
+
+let version t = t
--- src/ocaml.mli
+++ src/ocaml.mli
@@ -1,5 +1,9 @@
 open Import
 
-(** Returns the version returned by [ocamlc --version]. On every call, runs this
-    command, so try to cache the result if you need the version several times. *)
-val version : Sandbox.t -> (string, string) result Promise.t
+type t
+
+val make :
+     Sandbox.t
+  -> (t, [ `Ocamlc_missing | `Ocamlc_version_unexpected ]) result Promise.t
+
+val version_semver : t -> Ocaml_version.t
