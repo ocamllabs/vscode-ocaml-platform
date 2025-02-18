14c54b1c64408201f1af6e43aa85767f30b6c277
Pizie Dust <playersrebirth@gmail.com>
2025-02-18 09:20:00 +0100

    modify to include latest version

--- src/ocaml_lsp.mli
+++ src/ocaml_lsp.mli
@@ -3,7 +3,12 @@ open Import
 type t
 
 val of_initialize_result : LanguageClient.InitializeResult.t -> t
-val is_version_up_to_date : t -> Ocaml_version.t -> (unit, [ `Msg of string ]) result
+
+val is_version_up_to_date
+  :  t
+  -> Ocaml_version.t
+  -> (unit, [> `Msg of string * string ]) result
+
 val can_handle_switch_impl_intf : t -> bool
 val can_handle_infer_intf : t -> bool
 val can_handle_typed_holes : t -> bool
