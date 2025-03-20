bec5842323c46dd2e804f4af46f90788b1ea2a51
Pizie Dust <playersrebirth@gmail.com>
2025-03-20 18:55:16 +0100

    revert

--- src/ocaml_lsp.ml
+++ src/ocaml_lsp.ml
@@ -230,12 +230,11 @@ let is_version_up_to_date t ocaml_v =
           | None -> sprintf "to %s" new_
           | Some old -> sprintf "from %s to %s" old new_
         in
-        ( sprintf
-            "There is a newer version of ocaml-lsp-server available. Consider upgrading \
-             %s. Hint: $ opam install ocaml-lsp-server=%s and restart the lsp server"
-            upgrade
-            new_
-        , new_ )
+        sprintf
+          "There is a newer version of ocaml-lsp-server available. Consider upgrading \
+           %s. Hint: $ opam install ocaml-lsp-server=%s and restart the lsp server"
+          upgrade
+          new_
     in
     `Msg msg)
 ;;
--- src/ocaml_lsp.mli
+++ src/ocaml_lsp.mli
@@ -3,12 +3,7 @@ open Import
 type t
 
 val of_initialize_result : LanguageClient.InitializeResult.t -> t
-
-val is_version_up_to_date
-  :  t
-  -> Ocaml_version.t
-  -> (unit, [> `Msg of string * string ]) result
-
+val is_version_up_to_date : t -> Ocaml_version.t -> (unit, [ `Msg of string ]) result
 val can_handle_switch_impl_intf : t -> bool
 val can_handle_infer_intf : t -> bool
 val can_handle_typed_holes : t -> bool
