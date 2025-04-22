e4809049d90bc75b538c75751f24747b0c709c1e
Pizie Dust <playersrebirth@gmail.com>
2025-04-22 12:01:42 +0100

    fix minor bugs

--- src/sandbox.mli
+++ src/sandbox.mli
@@ -30,14 +30,14 @@ type t =
   | Esy of Esy.t * Esy.Manifest.t
   | Global
   | Custom of string
-  | Dune_pkg of string
+  | Dune of string
 
 val workspace_root : unit -> Path.t option
 val equal : t -> t -> bool
 val to_string : t -> string
 val to_pretty_string : t -> string
 val of_settings : unit -> t option Promise.t
-val detect_dune_pkg_lock_dir : project_root:Path.t -> unit -> t option Promise.t
+val detect_dune_lock_dir : project_root:Path.t -> unit -> t option Promise.t
 val detect : unit -> t option Promise.t
 val of_settings_or_detect : unit -> t option Promise.t
 val save_to_settings : t -> unit Promise.t
