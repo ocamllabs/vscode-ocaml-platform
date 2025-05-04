ad07ff0db02aa634477e5fbb6d3cd4b8d87c032a
Pizie Dust <playersrebirth@gmail.com>
2025-05-04 08:23:05 +0100

    rename dune_pkg to dune

--- src/sandbox.mli
+++ src/sandbox.mli
@@ -30,7 +30,7 @@ type t =
   | Esy of Esy.t * Esy.Manifest.t
   | Global
   | Custom of string
-  | Dune of Dune_pkg.t
+  | Dune of Dune.t
 
 val workspace_root : unit -> Path.t option
 val equal : t -> t -> bool
