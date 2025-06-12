2c041cb825c5aa019fa52c872655f8ebd461f022
Pizie Dust <playersrebirth@gmail.com>
2025-06-12 02:57:54 +0100

    add comments to dune interface file

--- src/dune.mli
+++ src/dune.mli
@@ -1,3 +1,4 @@
+(** Provide an interface to Dune pkg management.*)
 open Import
 
 type t =
@@ -5,10 +6,24 @@ type t =
   ; bin : Cmd.spawn
   }
 
+(** Check if a dune project has been locked.
+    If not, user should be advised to run `dune pkg lock` *)
 val is_project_locked : t -> bool Promise.t
+
+(** Check if the dune project has ocamllsp server in its dev-tools.
+    If not, user should be advised to run `dune tools exec ocamllsp` *)
 val is_ocamllsp_present : t -> bool Promise.t
+
+(** Generic function to execute dune commands *)
 val exec : t -> args:string list -> Cmd.t
+
+(** Execute any `dune tools exec` command*)
 val exec_tool : t -> tool:string -> ?args:string list -> unit -> Cmd.t
+
+(** Check if amy two instances of dune pkg management projects are the equal *)
 val equal : t -> t -> bool
+
 val make : root:Path.t -> unit -> t option Promise.t
+
+(** Get the path of the project root directory *)
 val root : t -> Path.t
