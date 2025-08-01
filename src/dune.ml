106a35b19f081ea9793273377cb81b69eaeafbaa
PixieDust <111846546+PizieDust@users.noreply.github.com>
2025-08-01 17:03:26 +0100

    Update src/dune.ml
    Co-authored-by: Sonja Heinze <sonjaleaheinze@gmail.com>

--- src/dune.ml
+++ src/dune.ml
@@ -91,7 +91,7 @@ let is_ocamllsp_present t =
   match ocamllsp_path with
   | Ok _path -> true
   | Error err ->
-    log_chan `Error ~section:"dune" "Ocamllsp not found: %s" err;
+    log_chan `Info ~section:"dune" "Ocamllsp not found" err;
     false
 ;;
 
