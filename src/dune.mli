4ca4c8f6f75963f6302417a7278f187f7c9f1de4
PixieDust <111846546+PizieDust@users.noreply.github.com>
2025-08-04 00:25:13 +0100

    Check if ocamllsp is present the dune way (#1907)
    * check if ocamllsp is present the dune way
    
    * add changelog
    
    * Update src/dune.ml
    
    Co-authored-by: Sonja Heinze <sonjaleaheinze@gmail.com>
    
    * Update src/dune.ml
    
    Co-authored-by: Sonja Heinze <sonjaleaheinze@gmail.com>
    
    * Update src/sandbox.ml
    
    Co-authored-by: Sonja Heinze <sonjaleaheinze@gmail.com>
    
    * Update src/dune.mli
    
    Co-authored-by: Sonja Heinze <sonjaleaheinze@gmail.com>
    
    * minor bug fix
    
    * install ocamllsp for dune
    
    * remove changelog
    
    * Update src/dune.mli
    
    Signed-off-by: Sora Morimoto <sora@morimoto.io>
    
    ---------
    
    Signed-off-by: Sora Morimoto <sora@morimoto.io>
    Co-authored-by: Sonja Heinze <sonjaleaheinze@gmail.com>
    Co-authored-by: Sora Morimoto <sora@morimoto.io>

--- src/dune.mli
+++ src/dune.mli
@@ -10,9 +10,6 @@ type t =
     If not, user should be advised to run `dune pkg lock` *)
 val is_project_locked : t -> bool Promise.t
 
-(** Check if the dune project has ocamllsp server as a dev-tool. *)
-val is_ocamllsp_present : t -> bool Promise.t
-
 (** Generic function to execute dune commands *)
 val command : t -> args:string list -> Cmd.t
 
@@ -22,8 +19,16 @@ val exec : target:string -> ?args:string list -> t -> Cmd.t
 (** Run specific `dune pkg <foo> commands*)
 val exec_pkg : cmd:string -> ?args:string list -> t -> Cmd.t
 
-(** Execute any `dune tools exec` command*)
-val exec_tool : tool:string -> ?args:string list -> t -> Cmd.t
+(** Run `dune tools <exec/which>` *)
+val tools
+  :  tool:string
+  -> ?args:string list
+  -> t
+  -> [< `Exec_ | `Which | `Install ]
+  -> Cmd.t
+
+(** Check if the dune project has ocamllsp server as a dev-tool. *)
+val is_ocamllsp_present : t -> bool Promise.t
 
 (** Check if amy two instances of dune pkg management projects are equal *)
 val equal : t -> t -> bool
--- src/sandbox.ml
+++ src/sandbox.ml
@@ -545,13 +545,20 @@ let select_sandbox_and_save t =
   Some sandbox
 ;;
 
-let get_command sandbox bin args (dune_cmd_type : [> `Tool | `Command | `Exec ]) : Cmd.t =
+let get_command
+      sandbox
+      bin
+      args
+      (dune_cmd_type : [> `Tool | `Command | `Exec | `Install ])
+  : Cmd.t
+  =
   match sandbox with
   | Opam (opam, switch) -> Opam.exec opam switch ~args:(bin :: args)
   | Esy (esy, manifest) -> Esy.exec esy manifest ~args:(bin :: args)
   | Dune dune ->
     (match dune_cmd_type with
-     | `Tool -> Dune.exec_tool ~tool:bin ~args dune
+     | `Install -> Dune.tools ~tool:bin ~args dune `Install
+     | `Tool -> Dune.tools ~tool:bin ~args dune `Exec_
      | `Command -> Dune.command dune ~args
      | `Exec -> Dune.exec ~target:bin ~args dune)
   | Global -> Spawn { bin = Path.of_string bin; args }
