8421f9c1427cc462d0d4178fa35231b36728fd3c
Marc Lasson <marc.lasson@lexifi.com>
2026-06-03 12:07:17 +0200

    Prefix Node runtime primitives with library name

--- src-bindings/node/node_stub.js
+++ src-bindings/node/node_stub.js
@@ -1,19 +1,19 @@
 'use strict';
 
-//Provides: node_child_process
-var node_child_process = require("node:child_process");
+//Provides: vscode_node_child_process
+var vscode_node_child_process = require("node:child_process");
 
-//Provides: node_constants
-var node_constants = require("node:constants");
+//Provides: vscode_node_constants
+var vscode_node_constants = require("node:constants");
 
-//Provides: node_fs
-var node_fs = require("node:fs/promises");
+//Provides: vscode_node_fs
+var vscode_node_fs = require("node:fs/promises");
 
-//Provides: node_net
-var node_net = require("node:net");
+//Provides: vscode_node_net
+var vscode_node_net = require("node:net");
 
-//Provides: node_os
-var node_os = require("node:os");
+//Provides: vscode_node_os
+var vscode_node_os = require("node:os");
 
-//Provides: node_path
-var node_path = require("node:path");
+//Provides: vscode_node_path
+var vscode_node_path = require("node:path");
