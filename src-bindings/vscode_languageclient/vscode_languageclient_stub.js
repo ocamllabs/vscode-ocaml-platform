f36afd31789505cafd1bcabbd64fc888cf866a88
David Sancho Moreno <dsnxmoreno@gmail.com>
2026-06-19 18:57:12 +0200

    Use node languageclient entrypoint

--- src-bindings/vscode_languageclient/vscode_languageclient_stub.js
+++ src-bindings/vscode_languageclient/vscode_languageclient_stub.js
@@ -1,4 +1,4 @@
 // oxlint-disable no-unused-vars
 
 // Provides: vscode_languageclient
-const vscode_languageclient = require("vscode-languageclient");
+const vscode_languageclient = require("vscode-languageclient/node");
