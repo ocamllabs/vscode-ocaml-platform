521c454f7ebf44a473d45416b17c0ca64ccbd05f
David Sancho Moreno <dsnxmoreno@gmail.com>
2026-06-22 16:11:23 +0200

    Add activation.test.js and run as part of CI

--- .vscode-test.js
+++ .vscode-test.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require("@vscode/test-cli");
+const path = require("node:path");
 
 module.exports = defineConfig({
   files: "tests/suite/**/*.test.js",
+  workspaceFolder: path.join(__dirname, "tests", "fixtures", "lsp"),
+  launchArgs: ["--disable-workspace-trust"],
+  mocha: {
+    timeout: 20000,
+  },
 });
